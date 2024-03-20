c876bc4193bd45d66f3f7d4f678fa4b66e7f5994
s18684 <s18684@pjwstk.edu.pl>
2024-03-20 13:51:27 +0100

    Dodano metode feature-max

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,3 +13,18 @@ static double CalculateAverage(int[] array)
 
     return (double)sum / array.Length;
 }
+
+static int FindMax(int[] array)
+{
+    if (array.Length == 0)
+        throw new ArgumentException("Array cannot be empty.");
+
+    int max = array[0];
+    foreach (int num in array)
+    {
+        if (num > max)
+            max = num;
+    }
+
+    return max;
+}
