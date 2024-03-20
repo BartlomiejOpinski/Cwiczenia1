abee3e5490d47b9db11ac3f8db962ce6df04aaf3
s18684 <s18684@pjwstk.edu.pl>
2024-03-20 13:46:47 +0100

    Usunieto niepotrzebne dane z metody

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,14 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
-
-int bob = 5;
-int rob = 10;
-
-Console.WriteLine(bob+rob);
-
-string hello = "hello";
-
 static double CalculateAverage(int[] array)
 {
     if (array.Length == 0)
